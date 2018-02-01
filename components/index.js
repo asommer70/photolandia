49e316305e07aa886c274eb5bf1575d02c1ef310
Adam Sommer <asommer70@gmail.com>
2018-02-01 18:33:20 -0500

    Integrated Photoswipe library for lightbox, paging, etc.

--- components/index.js
+++ components/index.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const https = require('https');
+const ncp = require('ncp').ncp;
+
+ncp.limit = 16;
 
 const assets =  '../assets/js/vendor/';
+const cssAssets =  '../assets/css/vendor/';
 
 if (!fs.existsSync(assets)){
     fs.mkdirSync(assets);
+}
+
+if (!fs.existsSync(cssAssets)){
+    fs.mkdirSync(cssAssets);
 }
 
 fs.createReadStream('./node_modules/foundation-sites/dist/js/foundation.min.js')
@@ -15,3 +23,21 @@ fs.createReadStream('./node_modules/jquery/dist/jquery.min.js')
 
   fs.createReadStream('./node_modules/mousetrap/mousetrap.min.js')
     .pipe(fs.createWriteStream(assets + 'mousetrap.min.js'));
+
+fs.createReadStream('./node_modules/photoswipe/dist/photoswipe.min.js')
+  .pipe(fs.createWriteStream(assets + 'photoswipe.min.js'));
+
+fs.createReadStream('./node_modules/photoswipe/dist/photoswipe-ui-default.min.js')
+  .pipe(fs.createWriteStream(assets + 'photoswipe-ui-default.min.js'));
+
+fs.createReadStream('./node_modules/photoswipe/dist/photoswipe.css')
+  .pipe(fs.createWriteStream(cssAssets + 'photoswipe.css'));
+
+  if (!fs.existsSync(cssAssets + '/default-skin')){
+      fs.mkdirSync(cssAssets + '/default-skin');
+  }
+ncp('./node_modules/photoswipe/dist/default-skin/', cssAssets + '/default-skin', function (err) {
+ if (err) {
+   console.error('ncp photoswipe/dist/default-skin err:', err);
+ }
+});
