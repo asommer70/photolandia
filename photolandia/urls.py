744e5474be1267350a821c9f58348a1db1ad7647
Adam Sommer <asommer70@gmail.com>
2018-02-12 11:43:26 -0500

    Created view function for /api/login to authenticate and get a User's token for API calls.

--- photolandia/urls.py
+++ photolandia/urls.py
@@ -1,18 +1,3 @@
-"""photolandia URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,7 +9,8 @@ urlpatterns = [
     path('albums/', include('albums.urls')),
     path('photos/', include('photos.urls')),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login, name='login'), 
+    path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, {'template_name': 'account/logout.html'}, name='logout'),
+    path('api/login', views.api_login, name="api_login"),
     path('', views.IndexView.as_view()),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photolandia/views.py
+++ photolandia/views.py
@@ -1,8 +1,30 @@
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login                    
+import json
+
 
 class IndexView(generic.RedirectView):
     url = '/photos/'
 
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
+
+
+@csrf_exempt  
+def api_login(request):
+	if request.method == 'POST':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+
+		user = authenticate(username=body['username'], password=body['password'])
+		if user:
+			return JsonResponse({'message': "Thank you for logging in.", 'token': user.auth_token.key})
+		else:
+			return JsonResponse({'message': 'Bad username or password.'})
+	else:
+		return HttpResponseRedirect(reverse('photos:list'))
